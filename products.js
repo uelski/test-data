a2407db7a33d5da673d70e7f6bb4569b81d82de8
Sam Vredenburgh <svredenburgh@aislerocket.com>
2020-03-21 12:15:59 -0600

    init setup

---
+++ products.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    "sku": "1122",
+    "producer": "Kaya",
+    "thc": "10%",
+    "strain": "Super Sour Lemon",
+    "price":"20",
+    "image": "./sslemon.jpg"
+  },
+  {
+    "sku": "2233",
+    "producer": "Kaya",
+    "thc": "20%",
+    "strain": "Blamo",
+    "price":"25",
+    "image": "./blamo.jpeg"
+  },
+  {
+    "sku": "3344",
+    "producer": "Kaya",
+    "thc": "15%",
+    "strain": "Mango Haze",
+    "price":"30",
+    "image": "./mangohaze.jpeg"
+  },
+  {
+    "sku": "4455",
+    "producer": "Kaya",
+    "thc": "12%",
+    "strain": "Dark Star",
+    "price":"35",
+    "image": "./darkstart.jpeg"
+  },
+  {
+    "sku": "5566",
+    "producer": "Kaya",
+    "thc": "30%",
+    "strain": "Triple Diesel",
+    "price":"40",
+    "image": ".tdiesel.jpeg"
+  },
+  {
+    "sku": "6677",
+    "producer": "Kaya",
+    "thc": "50%",
+    "strain": "Dreamwrecker",
+    "price":"45",
+    "image": "./dreamwrecker.jpeg"
+  }
+]
